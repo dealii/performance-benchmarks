150468e4d5d79743a4303c5dc66f61b7f3dd55d3
Timo Heister <timo.heister@gmail.com>
2020-10-12 10:33:04 -0400

    disable taskflow

--- test.sh
+++ test.sh
@@ -31,6 +31,7 @@ cd $BUILDDIR
 echo hi from `pwd` >>$logfile
 cmake -G "Ninja" \
       -D DEAL_II_COMPONENT_EXAMPLES=OFF \
+      -D DEAL_II_WITH_TASKFLOW=OFF \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_INSTALL_PREFIX=`pwd`/install \
       $basepath/dealii >>$logfile 2>&1 && nice ninja install >>$logfile 2>&1 || exit -1
